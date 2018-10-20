be622881c6bd2d11cc35ab332114575f36bb3f00
new name <new email>
2018-10-20 13:59:42 +0700

    Them file .h

---
+++ DoAn.h
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include <stdio.h>
+#include <conio.h>
+#include <iomanip>
+#include <math.h>
+#include <iostream>
+#include "Lap.h"
+#include "Khang.h"
+
+#define max 100
+#define printstr(w)
+
+
+using namespace std;
